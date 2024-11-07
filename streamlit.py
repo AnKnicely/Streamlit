4fbf5716d898a657fada166574129ea3a681b768
AnKnicely <151943483+AnKnicely@users.noreply.github.com>
2024-11-06 19:59:12 -0500

    Update streamlit.py
    fixing bugs

--- streamlit.py
+++ streamlit.py
@@ -6,7 +6,7 @@ st.title("College Loan Payment App")
 
 st.write("### Input Data")
 col1, col2 = st.columns(2)
-loan_value = col1.number_input("Loan Value", min = 0, value = 100000)
+loan_value = col1.number_input("Loan Value", min_value = 0, value = 100000)
 deposit = col1.number_input("Deposit", min_value = 0, value =200)
 interest_rate = col2.number_input("Interest Rate (in %)", min_value = 0.0, value = 5.5)
 loan_term = col2.number_input("Loan Term (in years)", min_value = 1, value = 10)
